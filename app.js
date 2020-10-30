64701fef160e79602877a6186fbe77d81e37ac5f
AsgharB <abhutta@Asghar.local>
2020-10-30 18:46:17 -0400

    exports module and added some stuff I guess

--- app.js
+++ app.js
@@ -1,28 +1,42 @@
-var commandLineArgs = process.argv;
-console.log(process.argv)
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
+const profileDataArgs = process.argv.slice(2);
+const [name, github] = profileDataArgs; //matches 1 to 1 using index name = [0] which comes from command line input
+
+// var commandLineArgs = process.argv;
+// console.log(process.argv)
 
 
-process.argv[1];
-console.log(process.argv[1]);
+// process.argv[1];
+// console.log(process.argv[1]);
 
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
 
-const printProfileData = profileDataArr => { //why no brackets around profileDataArr - only 1 argument is being passed
-        // This...
-        // for (let i = 0; i < profileDataArr.length; i += 1) {
-        //   console.log(profileDataArr[i]);
-        // }
+  fs.writeFile('index.html', generatePage(name, github), err => {
+    if (err) throw err;
+  
+    console.log('Portfolio complete! Check out index.html to see the output!');
+  });
+
+
+
+
+// console.log(profileDataArgs);
+
+// const printProfileData = profileDataArr => { //why no brackets around profileDataArr - only 1 argument is being passed
+//         // This...
+//         // for (let i = 0; i < profileDataArr.length; i += 1) {
+//         //   console.log(profileDataArr[i]);
+//         // }
       
-        console.log('================');
+//         console.log('================');
       
-        // Is the same as this...
-        profileDataArr.forEach((profileItem) => {
-          console.log(profileItem)
-        });
-      };
+//         // Is the same as this...
+//         profileDataArr.forEach((profileItem) => {
+//           console.log(profileItem)
+//         });
+//       };
 
-printProfileData(profileDataArgs);
+// printProfileData(profileDataArgs);
 
 
 // // Using function expression syntax
